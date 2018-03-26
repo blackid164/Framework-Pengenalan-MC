6596d3db87b0a0eeb3711e47a320fa561ba8ebb8
blackid164 <37794504+blackid164@users.noreply.github.com>
2018-03-27 00:43:17 +0700

    Edit Blom Selesai

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -67,13 +67,11 @@
         <!-- Indicators -->
         <ol class="carousel-indicators visible-xs">
           <li data-target="#carousel-slider" data-slide-to="0" class="active"></li>
-          <li data-target="#carousel-slider" data-slide-to="1"></li>
-          <li data-target="#carousel-slider" data-slide-to="2"></li>
         </ol>
 
         <div class="carousel-inner">
           <div class="item active">
-            <img src="img/1.jpg" class="img-responsive" alt="">
+            <img src="assets/img/1.jpg" class="img-responsive" alt="">
             <div class="carousel-caption">
               <div class="wow fadeInUp" data-wow-offset="0" data-wow-delay="0.3s">
                 <h2><span>Politeknik Negeri Malang</span></h2>
@@ -88,39 +86,17 @@
               <div class="wow fadeInUp" data-wow-offset="0" data-wow-delay="0.9s">
                 <form class="form-inline">
                   <div class="form-group">
-                    <button type="livedemo" name="Live Demo" class="btn btn-primary btn-lg" required="required">Live Demo</button>
+                    <button type="livedemo" name="Live Demo" class="btn btn-primary btn-lg" required="required">Akademik</button>
                   </div>
                   <div class="form-group">
-                    <button type="getnow" name="Get Now" class="btn btn-primary btn-lg" required="required">Get Now</button>
+                    <button type="getnow" name="Get Now" class="btn btn-primary btn-lg" required="required">Jurusan</button>
                   </div>
                 </form>
               </div>
             </div>
           </div>
 
-          <div class="item">
-            <img src="img/2.jpg" class="img-responsive" alt="">
-            <div class="carousel-caption">
-              <div class="wow fadeInUp" data-wow-offset="0" data-wow-delay="1.0s">
-                <h2>Fully Responsive</h2>
-              </div>
-              <div class="col-md-10 col-md-offset-1">
-                <div class="wow fadeInUp" data-wow-offset="0" data-wow-delay="0.6s">
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing</p>
-                </div>
-              </div>
-              <div class="wow fadeInUp" data-wow-offset="0" data-wow-delay="1.6s">
-                <form class="form-inline">
-                  <div class="form-group">
-                    <button type="livedemo" name="purchase" class="btn btn-primary btn-lg" required="required">Live Demo</button>
-                  </div>
-                  <div class="form-group">
-                    <button type="getnow" name="subscribe" class="btn btn-primary btn-lg" required="required">Get Now</button>
-                  </div>
-                </form>
-              </div>
-            </div>
-          </div>
+          
         </div>
 
         <a class="left carousel-control hidden-xs" href="#carousel-slider" data-slide="prev">
@@ -223,7 +199,7 @@
   <script>
     wow = new WOW({}).init();
   </script>
-  <script src="contactform/contactform.js"></script>
+  <script src="assets/contactform/contactform.js"></script>
 
 </body>
 
